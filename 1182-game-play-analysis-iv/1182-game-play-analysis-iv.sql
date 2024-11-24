900403d388a8831aaa4a488891398d76ba278ff9
Omar Mohamed <om1963448@gmail.com>
2024-11-24 19:46:38 +0200

    Time: 3143 ms (5.04%), Space: 0B (100%) - LeetHub

--- 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -1,9 +1,14 @@
 /* Write your T-SQL query statement below */
-select round(count(distinct f.player_id)*1.00/(select count(distinct player_id) from Activity) , 2) as fraction
-from (
-  select player_id, dateadd(day, 1, min(event_date)) as next_date
-  from Activity
-  group by player_id
-) f
-inner join Activity a
-on f.player_id = a.player_id and f.next_date = a.event_date
+SELECT
+    ROUND(COUNT(DISTINCT nt.player_id) * 1.00 / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) fraction
+FROM
+    (
+        SELECT player_id, DATEADD(DAY, 1, MIN(event_date)) AS next_date
+        FROM Activity
+        GROUP BY player_id
+    ) AS nt
+INNER JOIN
+       Activity a
+ON
+       nt.player_id = a.player_id
+       AND nt.next_date = a.event_date;
