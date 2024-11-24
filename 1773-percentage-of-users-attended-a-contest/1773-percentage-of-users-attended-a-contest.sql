b1897ba384b69604616f0f9c1c1aeb1a58473a88
Omar Mohamed <om1963448@gmail.com>
2024-11-24 16:46:55 +0200

    Time: 3112 ms (64.3%), Space: 0B (100%) - LeetHub

--- 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -1,7 +1,15 @@
 /* Write your T-SQL query statement below */
-SELECT r.contest_id, 
-       ROUND(COUNT(r.user_id) * 100.0 / (SELECT COUNT(*) FROM Users), 2) AS percentage
-FROM Register r
-JOIN Users u ON u.user_id = r.user_id
-GROUP BY r.contest_id
-ORDER BY percentage DESC, r.contest_id ASC;
+SELECT 
+    r.contest_id, 
+    ROUND(COUNT(r.user_id) * 100.0 / (SELECT COUNT(*) FROM Users), 2) AS percentage
+FROM
+    Register r
+JOIN
+    Users u
+    ON
+    u.user_id = r.user_id
+GROUP BY
+    r.contest_id
+ORDER BY
+    percentage DESC,
+    r.contest_id ASC;
