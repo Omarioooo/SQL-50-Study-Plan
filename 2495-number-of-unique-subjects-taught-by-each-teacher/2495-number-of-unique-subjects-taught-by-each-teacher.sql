383566876908220371ba0e32ea54c278a9822a8e
Omar Mohamed <om1963448@gmail.com>
2024-11-24 19:48:27 +0200

    Time: 1340 ms (97.73%), Space: 0B (100%) - LeetHub

--- 2495-number-of-unique-subjects-taught-by-each-teacher/2495-number-of-unique-subjects-taught-by-each-teacher.sql
+++ 2495-number-of-unique-subjects-taught-by-each-teacher/2495-number-of-unique-subjects-taught-by-each-teacher.sql
@@ -1,5 +1,8 @@
-# Write your MySQL query statement below
-SELECT teacher_id,
-       COUNT(DISTINCT subject_id) cnt
-FROM Teacher
-GROUP by teacher_id ;
+/* Write your T-SQL query statement below */
+SELECT 
+    teacher_id,
+    COUNT(DISTINCT subject_id) cnt
+FROM
+    Teacher
+GROUP by 
+    teacher_id ;
