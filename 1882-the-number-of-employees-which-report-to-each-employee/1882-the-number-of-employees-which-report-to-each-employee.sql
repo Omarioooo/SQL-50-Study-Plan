d1277015961503d606ed5f528b1b41dd27903180
Omar Mohamed <om1963448@gmail.com>
2024-11-24 20:29:33 +0200

    Time: 762 ms (78.35%), Space: 0B (100%) - LeetHub

---
+++ 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,17 @@
+/* Write your T-SQL query statement below */
+SELECT 
+    e1.employee_id,
+    e1.name,
+    COUNT(e2.employee_id) AS reports_count,
+    ROUND(AVG(CAST(e2.age AS FLOAT)), 0) AS average_age
+FROM 
+    Employees e1
+JOIN 
+    Employees e2 ON e1.employee_id = e2.reports_to
+GROUP BY 
+    e1.employee_id, e1.name
+HAVING 
+    COUNT(e2.employee_id) > 0
+ORDER BY 
+    e1.employee_id;
+
