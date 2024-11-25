e44fa7a307514fd089a9d936cad660cde4219feb
Omar Mohamed <om1963448@gmail.com>
2024-11-25 16:09:47 +0200

    Time: 1452 ms (5.14%), Space: 0B (100%) - LeetHub

--- 1480-movie-rating/1480-movie-rating.sql
+++ 1480-movie-rating/1480-movie-rating.sql
@@ -1,28 +1,48 @@
 /* Write your T-SQL query statement below */
-SELECT name AS results 
-FROM (
-    SELECT TOP 1 
+WITH user_with_max_ratings AS (
+    SELECT TOP (1) 
         u.name, 
-        COUNT(DISTINCT mr.movie_id) AS movie_count
-    FROM MovieRating mr
-    JOIN Users u 
+        movie_count = COUNT(DISTINCT mr.movie_id)
+    FROM 
+        MovieRating mr
+    JOIN
+        Users u 
         ON mr.user_id = u.user_id
-    GROUP BY mr.user_id, u.name
-    ORDER BY movie_count DESC, u.name
-) AS user_with_max_ratings
+    GROUP BY
+        mr.user_id,
+        u.name
+    ORDER BY
+        movie_count DESC,
+        u.name
+),
+movie_with_max_avg_rating AS (
+    SELECT TOP (1) 
+        m.title AS name,  -- Ensure the column name matches with the first CTE
+        AVG(CAST(mr.rating AS FLOAT)) AS avg_rating
+    FROM 
+        MovieRating mr
+    JOIN
+        Movies m 
+        ON mr.movie_id = m.movie_id
+    WHERE
+        MONTH(mr.created_at) = 2 
+        AND YEAR(mr.created_at) = 2020
+    GROUP BY
+        m.movie_id,
+        m.title
+    ORDER BY
+        avg_rating DESC,
+        m.title
+)
+
+SELECT
+    name AS results
+FROM 
+    user_with_max_ratings
 
 UNION ALL
 
-SELECT title 
-FROM (
-    SELECT TOP 1 
-        m.title, 
-        AVG(CAST(mr.rating AS FLOAT)) AS avg_rating
-    FROM MovieRating mr
-    JOIN Movies m 
-        ON mr.movie_id = m.movie_id
-    WHERE MONTH(mr.created_at) = 2 
-      AND YEAR(mr.created_at) = 2020
-    GROUP BY m.movie_id, m.title
-    ORDER BY avg_rating DESC, m.title
-) AS movie_with_max_avg_rating;
+SELECT
+    name AS results
+FROM
+    movie_with_max_avg_rating;
