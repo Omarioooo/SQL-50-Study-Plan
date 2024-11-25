633ec5b8ae749e5178fd641a288ebbea164aac59
Omar Mohamed <om1963448@gmail.com>
2024-11-25 14:46:36 +0200

    Time: 353 ms (99.76%), Space: 0B (100%) - LeetHub

---
+++ 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
@@ -0,0 +1,11 @@
+/* Write your T-SQL query statement below */
+SELECT 
+    e1.employee_id
+FROM
+    Employees e1
+WHERE
+    e1.salary < 30000 
+    AND
+    e1.manager_id NOT IN (SELECT employee_id FROM Employees )
+ORDER BY
+    e1.employee_id ; 
