1da7207a8784d9ad73999a6deae1939c816f9470
Omar Mohamed <om1963448@gmail.com>
2024-11-28 23:04:20 +0200

    Time: 262 ms (81.88%), Space: 0B (100%) - LeetHub

--- 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -1,8 +1,20 @@
-# Write your MySQL query statement below
-select m1.machine_id,
-       round(avg(m2.timestamp-m1.timestamp), 3) processing_time
-from Activity m1
-cross join Activity m2
-ON m1.machine_id = m2.machine_id AND m1.process_id = m2.process_id
-where m1.activity_type = 'start' and m2.activity_type ='end' 
-group by m1.machine_id ;       
+/* Write your T-SQL query statement below */
+SELECT 
+    m1.machine_id,
+    ROUND(AVG(m2.timestamp-m1.timestamp),3) AS processing_time
+FROM 
+    Activity m1
+INNER JOIN 
+    Activity m2
+ON 
+    m1.machine_id = m2.machine_id 
+    AND m1.process_id = m2.process_id
+WHERE 
+    m1.activity_type = 'start' 
+    AND 
+    m2.activity_type = 'end'
+GROUP BY 
+    m1.machine_id;
+
+
+
